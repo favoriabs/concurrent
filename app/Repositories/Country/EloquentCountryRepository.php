7be6c266cd03dd4d3ce2bb9e9af8bd8c8f4514ea
favour <favoriabs@gmail.com>
2016-10-03 03:05:53 +0100

    added 2 functions

--- app/Repositories/Country/EloquentCountryRepository.php
+++ app/Repositories/Country/EloquentCountryRepository.php
@@ -52,10 +52,24 @@
 			$from_value = $request->conv_from;
 			$from_country = Country::where('country_name', $convert_from)->first();
 			$to_country= Country::where('country_name', $convert_to)->first();
-			var_dump($from_country->currency_rate_todollar);
 			$dollar_value = (float)$from_value * $from_country->currency_rate_todollar;
-			$requested_value = $dollar_value * $to_country->currency_rate_todollar;
-			var_dump($requested_value);
+			$requested_value = $dollar_value / $to_country->currency_rate_todollar;
 			return $requested_value;
+		}
+
+		public function currencyNameFrom($request)
+		{
+			$convert_from = $request->curr_from;
+			var_dump($convert_from);
+			$from_country = Country::where('currency_code', $convert_from)->first();
+			var_dump($from_country);
+			return $from_country;
+		}
+
+		public function currencyNameTo($request)
+		{
+			$convert_to = $request->curr_to;
+			$to_country = Country::where('currency_code', $convert_to)->first();
+			return $to_country;
 		}
 	}
