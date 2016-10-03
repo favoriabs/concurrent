06be778d4afd98b7dfcae4d1cd5f279dede9b78c
favour <favoriabs@gmail.com>
2016-10-03 03:03:12 +0100

    tried to validate

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Repositories\Country\CountryContract;
+use JavaScript;
+use App\Country;
+use Session;
 
 class CountryController extends Controller
 {
@@ -16,18 +19,23 @@ class CountryController extends Controller
     	$this->repo = $countryMod;
     }
 
-    public function dropDown()
+    public function dropDown(Request $request)
     {
-    	$countries = $this->repo->findAll();
-    	return view('index')->with('countries', $countries);
+        $countries = $this->repo->findAll();
+    	return view('landingPage')->with('countries', $countries);
     }
+
 
     public function convertValues(Request $request) 
     {
+        // $this->validate($request, [
+        //         'conv_from' => 'required',
+        //     ]);
+        $countries = $this->repo->findAll();
         $values = $this->repo->convertFactor($request);
-        return view('convertedpage')->with('values' , $values);
-        // return back()
-        //     ->withInput()
-        //     ->with('values', $values);
+        // $countryCurrencyFrom = $this->repo->currencyNameFrom($request);
+        // $countryCurrencyTo = $this->repo->currencyNameTo($request);
+        Session::flash('message', 'Record Successfully added!');
+        return view('convertedpage')->with('values' , $values)->with('countries', $countries);
     }   
 }
